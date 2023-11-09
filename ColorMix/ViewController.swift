b5ff6b5e774628a87f5f90fff932244a6c6e49cf
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-09 09:50:31 -0500

    added switches for green and blue

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -11,17 +11,39 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
-        }
+        updateColor()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.backgroundColor = .black
+        updateColor()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
+        }
+        
+        if greenSwitch.isOn {
+            green = 1
+        }
+        
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        colorView.backgroundColor = color
     }
 }
