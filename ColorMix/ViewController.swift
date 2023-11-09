6bf0f2137f631fd803d0202fa7db9043c683b4f7
Christopher Lamm <christopher.lamm@companycam.com>
2023-11-09 09:32:04 -0500

    added switch that toggles the background color of the view from black to red

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -11,11 +11,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            colorView.backgroundColor = .red
+        } else {
+            colorView.backgroundColor = .black
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         colorView.backgroundColor = .black
     }
-
-
 }
-
